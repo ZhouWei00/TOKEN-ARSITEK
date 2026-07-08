40dbb23d9cf7846bf328aeca26c9ace4addd35d2
ZhouWei00 <164828282+ZhouWei00@users.noreply.github.com>
2026-07-08 21:36:23 +0700

    Create test/ARSToken.t.sol

---
+++ Contracts/test/ARSToken.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+/// @title ARSToken Tests
+/// @notice Unit tests for ARSToken.
+/// @dev Test cases will be implemented in later sprints.
+contract ARSTokenTest {
+
+}
