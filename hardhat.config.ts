a58b3dc0bc3d7ee06287ff00e286e39ec0d98139
ZhouWei00 <164828282+ZhouWei00@users.noreply.github.com>
2026-07-08 22:41:56 +0700

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,1 +1,16 @@
-export default {};
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.25",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
+export default config;
